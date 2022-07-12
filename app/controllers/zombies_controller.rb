2d714d07bcb1746d9c95bc56ec94bd756c718d62
chiku3 <p314chiku.y@gmail.com>
2022-07-12 07:16:54 +0000

    [ADD]refile

--- app/controllers/zombies_controller.rb
+++ app/controllers/zombies_controller.rb
@@ -1,7 +1,35 @@
 class ZombiesController < ApplicationController
+  def new
+    @zombie = Zombie.new
+  end
+  
+  def create
+    @zombie = Zombie.new(zombie_params)
+    @zombie.save
+    redirect_to zombie_path(@zombie)
+  end
+  
   def show
+  end
+  
+  def edit
+  end
+  
+  def updated
   end
 
   def index
+    @zombies = Zombie.all
   end
+  
+  def destroy
+  end
+  
+  
+  private
+
+  def zombie_params
+    params.require(:zombie).permit(:name, :body, :image, :tag_ids[])
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# サンプルデータ
+  5.times do |n|
+    Zombie.create!(
+      name: "ゾンビ#{n + 1}",
+      body: "概要説明",
+      image_id: "no_image.jpg"
+    )
+  end
+  
+# サンプルデータ
+  5.times do |n|
+    Tag.create!(
+      name: "タグ#{n + 1}"
+    )
+  end
