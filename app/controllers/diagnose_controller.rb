86f4d9ba876e488585d2d7717b6ae56f6c16cbc6
chiku3 <p314chiku.y@gmail.com>
2022-07-14 03:15:01 +0000

    [UPDATE]診断機能

--- app/controllers/diagnose_controller.rb
+++ app/controllers/diagnose_controller.rb
@@ -5,49 +5,45 @@ class DiagnoseController < ApplicationController
   def index
     # @zombies = Zombie.includes(:tags).where(tags: {name: "映画"}).or(Zombie.includes(:tags).where(tags: {name: "ドラマ"}))
 
-    if params[:q_1] == 'a'
-      @zombies_1 = Zombie.includes(:tags).where(tags: {name: "映画"}).or(Zombie.includes(:tags).where(tags: {name: "ドラマ"}))
-    elsif params[:q_1] == 'b'
-      @zombies_1 = Zombie.includes(:tags).where(tags: {name: "ゲーム"}).or(Zombie.includes(:tags).where(tags: {name: "スマホアプリ"}))
+    #ゾンビと何したい？
+    if params[:q_1] == 'a' #ゾンビを鑑賞したい
+      zombies_1 = Zombie.includes(:tags).where(tags: {name: "映画"}).or(Zombie.includes(:tags).where(tags: {name: "ドラマ"}))
+    elsif params[:q_1] == 'b' #ゾンビと遊びたい
+      zombies_1 = Zombie.includes(:tags).where(tags: {name: "ゲーム"}).or(Zombie.includes(:tags).where(tags: {name: "スマホアプリ"}))
     end
-
-    if params[:q_2] == 'a'
-      @zombies_2 = Zombie.includes(:tags).where(tags: {name: "こわい"}).or(Zombie.includes(:tags).where(tags: {name: "ホラー"}))
-    elsif params[:q_2] == 'b'
-      @zombies_2 = Zombie.includes(:tags).where(tags: {name: "かわいい"}).or(Zombie.includes(:tags).where(tags: {name: "恋愛"}))
-    end
-
-    s1 = @zombies_1.ids.uniq
-    s2 = @zombies_2.ids.uniq
-    total = s1.push(s2)
-    total.flatten!
-    @total_s = total.select{ |e| total.count(e) == 2 }.uniq
-    @zombies = Zombie.where id: @total_s
+    s1 = zombies_1.ids.uniq #該当するゾンビidの配列
     
+    # どっちのゾンビが好き？
+    if params[:q_2] == 'a' #怖いゾンビ
+      zombies_2 = Zombie.includes(:tags).where(tags: {name: "こわい"}).or(Zombie.includes(:tags).where(tags: {name: "ホラー"}))
+    elsif params[:q_2] == 'b' #かわいいゾンビ
+      zombies_2 = Zombie.includes(:tags).where(tags: {name: "かわいい"}).or(Zombie.includes(:tags).where(tags: {name: "シュール"}))
+    end
+    s2 = zombies_2.ids.uniq #該当するゾンビidの配列
+    
+    # ゾンビと何したい？
+    if params[:q_3] == 'a' #ゾンビと戦いたい
+      zombies_3 = Zombie.includes(:tags).where(tags: {name: "パニック"}).or(Zombie.includes(:tags).where(tags: {name: "アクション"}))
+    elsif params[:q_3] == 'b' #ゾンビと仲良くなりたい
+      zombies_3 = Zombie.includes(:tags).where(tags: {name: "恋愛"}).or(Zombie.includes(:tags).where(tags: {name: "青春"}))
+    end
+    s3 = zombies_3.ids.uniq #該当するゾンビidの配列
+    
+    # ゾンビとの接し方は？
+    if params[:q_4] == 'a' #サクッと
+      zombies_4 = Zombie.includes(:tags).where(tags: {name: "映画"}).or(Zombie.includes(:tags).where(tags: {name: "スマホアプリ"}))
+    elsif params[:q_4] == 'b' #長期的に
+      zombies_4 = Zombie.includes(:tags).where(tags: {name: "ドラマ"}).or(Zombie.includes(:tags).where(tags: {name: "ゲーム"}))
+    end
+    s4 = zombies_4.ids.uniq #該当するゾンビidの配列
+    
+    #質問から取得したゾンビidの配列を結合
+    total = s1 << s2 << s3 << s4
+    total.flatten!
+    #「重複回数＝質問の数」のidを取得
+    total_s = total.select{ |e| total.count(e) == 4 }.uniq
+    @zombies = Zombie.where id: total_s
 
-
-    # zombies = Zombie.all
-    # selected_tag_ids = params[:q_04]
-    # @mach_tag_count = 0
-    # tmp_mach_tag_count = 0
-    # @mached_zombies = []
-
-    # zombies.each do |zombie|
-    #   tmp_mach_tag_count = 0
-    #   zombie.tags.each do |tag|
-    #     if selected_tag_ids&.include?(tag.id.to_s)
-    #       tmp_mach_tag_count = tmp_mach_tag_count + 1
-    #     end
-    #   end
-
-    #   if @mach_tag_count < tmp_mach_tag_count
-    #     @mached_zombies = []
-    #     @mached_zombies << zombies
-    #     @mach_tag_count = tmp_mach_tag_count
-    #   elsif @mach_tag_count == tmp_mach_tag_count
-    #     @mached_zombies << zombies
-    #   end
-    # end
   end
 
   private
